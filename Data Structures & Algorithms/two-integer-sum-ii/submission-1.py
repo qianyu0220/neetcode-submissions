dbca325f717dab2584aaa0013951ff0679232fd9
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-08-26 22:28:34 -0700

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        n = len(numbers)
+        left, right = 0, n-1
+        while left < right:
+            total = numbers[left] + numbers[right]
+            if total == target:
+                return [left+1, right+1]
+            elif total < target:
+                left += 1
+            else:
+                right -= 1
